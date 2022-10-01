f265c61929620d62c403d2c3dcb3ea54f9b533b4
Tomáš Turek <tom.turek@atlas.cz>
2022-10-01 21:00:32 +0200

    update autobuildu

--- autobuild.sh
+++ autobuild.sh
@@ -2,41 +2,51 @@
 
 set -ueo pipefail
 
+install=0
+run=0
+clean=0
+
+config_file=build/.x++config
+
+for var in $@; do
+	if [ $var == run ]; then
+		run=1
+	fi
+	if [ $var == install ]; then
+		install=1
+	fi
+	if [ $var == clean ]; then
+		clean=1
+	fi
+done
+
+if [ $clean == 1 ]; then
+	rm -r build
+fi
+
 if [ -d xtodo/ ]; then
 	cp resources/icon.ico xtodo/icon.ico
 	if [ -d build ]; then
-		echo "Existing build dir"
+		echo "Existing build dir, skipping initialisation."
 	else 
 		mkdir build
-		touch build/todo.txt
-		./config.sh todo.txt > build/.x++config
+		echo "First task"  > build/todo.txt
+		echo "#script config after init" > $config_file
+		echo "CLIENT=GUI" > $config_file
+		echo "FILE=todo.txt" > $config_file
 	fi
 	cmake -S xtodo/ -B build/
 	cp resources/icon.ico build/icon.ico
 	cd build/
 	make
-
-	for var in $@; do
-		if [ $var == run ]; then
-			./xtodo
-		fi
-		if [ $var == install ]; then
-			cd ..
-			if [ -d /opt/xtodo ]; then
-				sudo cp -r build/* /opt/xtodo
-			else
-				sudo mkdir /opt/xtodo
-				sudo cp -r build/* /opt/xtodo
-			fi
-			cd build
-		fi
-		if [ $var == clean ]; then
-			cd ..
-			rm -r build
-		fi
-	done
+	if [ $install == 1 ]; then
+		sudo mkdir -p /opt/xtodo
+		sudo cp -r . /opt/xtodo/
+	fi
+	if [ $run == 1 ]; then
+		./xtodo
+	fi
 fi
-
 
 # Win-Build
 # x86_64-w64-mingw32-cmake -S x++todo/ -B win-build/
--- config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ueo pipefail
-
-echo "#script build"
-echo CLIENT=GUI
-echo FILE=todo.txt
