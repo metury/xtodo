5ee4f470de2765c39b74586b44a0e66803242d8c
Tomáš Turek <tom.turek@atlas.cz>
2024-11-29 19:10:59 +0100

    simple structure

--- tudu/src/main.rs
+++ tudu/src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+use regex::Regex;
+
+struct Task {
+    completion: bool,
+    priority: u16,
+    creation_date: i64,
+    completion_date: i64,
+    text: String,
+    project: String,
+    context: String,
+    other_values: HashMap<String, String>,
+    deletion: bool,
+}
+
+impl Task {
+    fn new(text: String) -> Task {
+    }
+}
+
+
+fn parse(line: &String) -> Option<Task> {
+    None
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
